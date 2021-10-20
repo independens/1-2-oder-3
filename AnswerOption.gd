4a25b9bcfe41bd996d19b6308bda532c9a970c2f
Max Jöhnk <maxjoehnk@gmail.com>
2021-10-20 21:52:53 +0200

    improve ui

--- AnswerOption.gd
+++ AnswerOption.gd
@@ -8,6 +8,10 @@ export var default_color = Color("343434")
 export var number: int = 1
 
 func _process(delta):
+	if text == "":
+		$Text.hide()
+	else:
+		$Text.show()
 	$Text.set_text(text)
 	$Background.set_frame_color(_get_color())
 	$Sprite.set_texture(number_texture)
